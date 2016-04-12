01cc714c0b44a4aa20b7e4c0cb3b74ca8ffdfdd5
edosetiawan1994 <edosetiawan194@gmail.com>
2016-04-12 23:29:02 +0700

    'deletefiles'

--- app/src/main/java/com/kala/kala/Callback.java
+++
@@ -1,8 +0,0 @@
-package com.kala.kala;
-
-/**
- * Created by Kala on 2/29/16.
- */
-public interface Callback<T> {
-    public void callback(Error error, T result);
-}
--- app/src/main/java/com/kala/kala/list.java
+++
@@ -1,8 +0,0 @@
-package com.kala.kala;
-
-/**
- * Created by Edo on 2/12/2016.
- */
-public interface list {
-    public boolean isHeader();
-}
